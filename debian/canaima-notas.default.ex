66bdc1b7fe9f0da0468eb12f3d2e13a4ec26cd2d
Francisco Javier Vasquez Guerrero <fvasquez@canaima.softwarelibre.gob.ve>
2010-08-31 14:19:55 -0430

    debianizamos el chichero de trabajo

---
+++ debian/canaima-notas.default.ex
@@ -0,0 +1,10 @@
+# Defaults for canaima-notas initscript
+# sourced by /etc/init.d/canaima-notas
+# installed at /etc/default/canaima-notas by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(canaima-notas):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="canaima-notas" command="/usr/bin/canaima-notas"
