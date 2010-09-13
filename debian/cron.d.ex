717b167ab81044e6e3c0eb8d20ff4d8bf1a7fb74
Francisco Vasquez <franjvasquezg@gmail.com>
2010-09-13 07:28:39 -0430

    retomando el codigo y confirmando que todo esta bien

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the canaima-notas package
-#
-0 4	* * *	root	[ -x /usr/bin/canaima-notas_maintenance ] && /usr/bin/canaima-notas_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(canaima-notas):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="canaima-notas" command="/usr/bin/canaima-notas"
